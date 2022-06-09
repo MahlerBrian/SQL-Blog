770de9bd74a08bf7aecdfdec6c5dfcd11b4d963d
Brian Albert <brian.david.albert@gmail.com>
2022-06-09 17:37:30 -0400

    connected mysql db

---
+++ routes/blog.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const db = require('../data/database');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.redirect('/posts');
+});
+
+router.get('/posts', (req, res) => {
+    res.render('posts-list');
+});
+
+router.get('/new-post', async (req, res) => {
+    const [ authors ] = await db.query('SELECT * FROM authors');
+    res.render('create-post', { authors: authors });
+});
+
+
+
+module.exports = router;
