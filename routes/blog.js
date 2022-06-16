b47d16dc441ed02400936a358c4a93fc5982d145
Brian Albert <brian.david.albert@gmail.com>
2022-06-16 05:53:44 -0400

    added crud routes with query parameters for get and put requests

--- routes/blog.js
+++ routes/blog.js
@@ -8,8 +8,11 @@ router.get('/', (req, res) => {
     res.redirect('/posts');
 });
 
-router.get('/posts', (req, res) => {
-    res.render('posts-list');
+router.get('/posts', async (req, res) => {
+    const query = `SELECT posts.*, authors.name AS author_name FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id`;
+    const [posts] = await db.query(query);
+    res.render('posts-list', { posts: posts });
 });
 
 router.get('/new-post', async (req, res) => {
@@ -17,6 +20,15 @@ router.get('/new-post', async (req, res) => {
     res.render('create-post', { authors: authors });
 });
 
-
+router.post('/posts', async (req, res) => {
+    const data = [
+        req.body.title,
+        req.body.summary,
+        req.body.content,
+        req.body.author
+    ];
+await    db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)', [data]);
+res.redirect('/posts');
+});
 
 module.exports = router;
